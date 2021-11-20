279cee88b5e8b6d245286f9acd0743140427a996
RobsDuarte <robsduar@gmail.com>
2021-11-20 10:40:25 -0300

    class request created

---
+++ source/Http/Request.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Source\Http;
+
+class Request 
+{
+    /**
+     * Método da minha requisição para o server (GET,POST,PUT... etc)
+     *
+     * @var string
+     */
+    private $httpMethod;
+
+    /**
+     * URI da minha requisição
+     *
+     * @var array
+     */
+    private $URI;
+
+    /**
+     * Cabeçalho da requisição
+     *
+     * @var array
+     */
+    private $headers;
+
+    /**
+     * Construtor da classe
+     */
+    public function __construct()
+    {
+        $this->httpMethod = $_SERVER["REQUEST_METHOD"];
+        $this->URI = $_SERVER["REQUEST_URI"] ?? [];
+        $this->headers = getallheaders();
+    }
+
+    /**
+     * Método responsável por retornar o método da requisição
+     *
+     * @return string
+     */
+    public function getHttpMethod()
+    {
+        return $this->httpMethod;
+    }
+
+    /**
+     * Método responsável por retornar a URI da página
+     *
+     * @return array
+     */
+    public function getURI()
+    {
+        return $this->URI;
+    }
+
+    /**
+     *  Método responsável por retornar o header da requisição
+     *
+     * @return array
+     */
+    public function getHeaders()
+    {
+        return $this->headers;
+    }
+
+}
