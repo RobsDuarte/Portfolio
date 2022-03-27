ae672b3e174df44bdbc5783876683d2da5f101e5
RobsDuarte <robsduar@gmail.com>
2022-03-27 00:10:18 -0300

    url to my github changed

--- Includes/Url.php
+++ Includes/Url.php
@@ -2,5 +2,5 @@
 
 $git_web_ivana = 'https://github.com/RobsDuarte/Website-Ivana';
 $web_ivana     = 'http://localhost/projetos/Website-Ivana/';
-$my_github     = 'https://github.com/RobsDuarte?tab=repositories';
+$my_github     = 'https://github.com/RobsDuarte';
 $my_linkedin   = 'https://www.linkedin.com/in/RobsDuarte';
